32db0b76696aaa9f42ab099c93156c16ce236f6b
J. Nick Koston <nick@home-assistant.io>
2026-09-09 16:08:23 +0200

    Use cipher category ids in the cipherstate error path test

--- tests/unit/test-cipherstate.c
+++ tests/unit/test-cipherstate.c
@@ -265,14 +265,14 @@ static void cipherstate_check_errors(void)
     compare(noise_cipherstate_get_cipher_id(0), NOISE_CIPHER_NONE);
     compare(noise_cipherstate_get_key_length(0), 0);
     compare(noise_cipherstate_get_mac_length(0), 0);
-    compare(noise_cipherstate_new_by_id(0, NOISE_HASH_SHA256),
+    compare(noise_cipherstate_new_by_id(0, NOISE_CIPHER_CHACHAPOLY),
             NOISE_ERROR_INVALID_PARAM);
     compare(noise_cipherstate_new_by_name(0, "ChaChaPoly"),
             NOISE_ERROR_INVALID_PARAM);
 
     /* If the id/name is unknown, the state parameter should be set to NULL */
     state = (NoiseCipherState *)8;
-    compare(noise_cipherstate_new_by_id(&state, NOISE_HASH_SHA256),
+    compare(noise_cipherstate_new_by_id(&state, NOISE_ID('C', 200)),
             NOISE_ERROR_UNKNOWN_ID);
     verify(state == NULL);
     state = (NoiseCipherState *)8;
